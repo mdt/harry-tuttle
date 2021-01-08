043c01e1f5420b650cf91948ab7b906cd78d2f19
Marc Tanner <marc.tanner@gmail.com>
2021-01-08 11:30:33 -0500

    Handle channels moved to or from 'no category', e.g. when their category is deleted

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -5,20 +5,23 @@ const csfunctions = require("../modules/csfunctions.js")
 /// communicate channel renames to the DB
 module.exports = async (client, oldChannel, newChannel) => {
 	if (oldChannel.type != "voice" || newChannel.type != "voice") { return; }
+	if (oldChannel.type !== "voice" || newChannel.type !== "voice") { return; }
 
 	let needs_sort = false;
 	let ocSlugged = slugify(oldChannel.name)
 	let ncSlugged = slugify(newChannel.name)
-	if (ocSlugged != ncSlugged) {
+	if (ocSlugged !== ncSlugged) {
 		const updates = channelstats.rename_channel(ocSlugged, ncSlugged);
 		needs_sort = true;
 		client.logger.log(`Channel rename from ${oldChannel.name} to ${newChannel.name} updated ${updates} rows`);
 	}
 
-	if (oldChannel.parent != newChannel.parent) {
-		const updates = channelstats.recategorize_channel(ncSlugged, newChannel.parent.name)
+	if (oldChannel.parent !== newChannel.parent) {
+		const oldCategoryName = oldChannel.parent ? oldChannel.parent.name : '[no category]';
+		const newCategoryName = newChannel.parent ? newChannel.parent.name : '[no category]';
+		const updates = channelstats.recategorize_channel(ncSlugged, newCategoryName);
 		needs_sort = true;
-		client.logger.log(`Channel recategorize from ${oldChannel.parent.name} to ${newChannel.parent.name} updated ${updates} rows`);
+		client.logger.log(`Channel recategorize from ${oldCategoryName} to ${newCategoryName} updated ${updates} rows`);
 	}
 
 	// re-sort the channels in its category
