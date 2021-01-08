50a8be2c1cfc26b9a6e83436db2db00be9798ebb
Marc Tanner <marc.tanner@gmail.com>
2021-01-08 11:27:19 -0500

    Reformat whitespace to match conventions

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -4,25 +4,25 @@ const csfunctions = require("../modules/csfunctions.js")
 
 /// communicate channel renames to the DB
 module.exports = async (client, oldChannel, newChannel) => {
-	 if (oldChannel.type != "voice" || newChannel.type != "voice") { return; }
+	if (oldChannel.type != "voice" || newChannel.type != "voice") { return; }
 
-	 let needs_sort = false;
-	 let ocSlugged = slugify(oldChannel.name)
-	 let ncSlugged = slugify(newChannel.name)
-	 if (ocSlugged != ncSlugged) {
-		  const updates = channelstats.rename_channel(ocSlugged, ncSlugged);
-		  needs_sort = true;
-		  client.logger.log(`Channel rename from ${oldChannel.name} to ${newChannel.name} updated ${updates} rows`);
-	 }
+	let needs_sort = false;
+	let ocSlugged = slugify(oldChannel.name)
+	let ncSlugged = slugify(newChannel.name)
+	if (ocSlugged != ncSlugged) {
+		const updates = channelstats.rename_channel(ocSlugged, ncSlugged);
+		needs_sort = true;
+		client.logger.log(`Channel rename from ${oldChannel.name} to ${newChannel.name} updated ${updates} rows`);
+	}
 
-	 if (oldChannel.parent != newChannel.parent) {
-		  const updates = channelstats.recategorize_channel(ncSlugged, newChannel.parent.name)
-		  needs_sort = true;
-		  client.logger.log(`Channel recategorize from ${oldChannel.parent.name} to ${newChannel.parent.name} updated ${updates} rows`);
-	 }
-	 
-	 // re-sort the channels in its category
-	 if (needs_sort && newChannel.parent) {
-		  csfunctions.sort_category(client, newChannel.guild.channels, newChannel.parent);
-	 }
+	if (oldChannel.parent != newChannel.parent) {
+		const updates = channelstats.recategorize_channel(ncSlugged, newChannel.parent.name)
+		needs_sort = true;
+		client.logger.log(`Channel recategorize from ${oldChannel.parent.name} to ${newChannel.parent.name} updated ${updates} rows`);
+	}
+
+	// re-sort the channels in its category
+	if (needs_sort && newChannel.parent) {
+		csfunctions.sort_category(client, newChannel.guild.channels, newChannel.parent);
+	}
 }
